bc49317194453eedd23e85edaebb3dc6df015be9
Pranay <oppranay666@gmail.com>
2024-03-19 21:05:01 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,55 @@
+# print("Hello world 12")
+# print(q)
+# import time
+# timestamp = time.strftime('%H:%M:%S')
+# print(timestamp)
+# timestamp = time.strftime('%H')
+# print(timestamp)
+# timestamp = time.strftime('%M')
+# print(timestamp)
+# timestamp = time.strftime('%S')
+# print(timestamp)
+# https://docs.python.org/3/library/time.html#time.strftime
+import time
+
+# def greet():
+#     current_hour = time.localtime().tm_hour
+
+#     if 5 <= current_hour < 12:
+#         return "Good Morning"
+#     elif 12 <= current_hour < 18:
+#         return "Good Afternoon"
+#     else:
+#         return "Good Evening"
+
+# if __name__ == "__main__":
+#     greeting = greet()
+#     print(f"{greeting}, User!")
+
+# colors = ["voilet", "indigo", "blue", "green"]
+# colors.sort()
+# print(colors)
+
+# num = [4,2,5,3,6,1,2,1,2,8,9,7]
+# num.sort()
+# print(num)
+
+# ex=[5,3,67,43,67]
+# ex.sort()
+# print(ex)
+
+# colors = ["voilet", "indigo", "blue", "green"]
+# colors.sort(reverse=True)
+# print(colors)
+
+# num = [4,2,5,3,6,1,2,1,2,8,9,7]
+# num.sort(reverse=True)
+# print(num)
+
+
+#tuple###
+
+tup=(1,5,6)
+print(type(tup))
+
+
